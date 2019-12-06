287cff21e384c1a7c90f4eabae565a34e0cf5b59
cjdcordeiro <cristovao.cordeiro@sixsq.com>
2019-12-06 14:30:31 +0100

    also remove volumes

--- watcher.sh
+++ watcher.sh
@@ -5,6 +5,11 @@ set -x
 docker events --filter 'event=die' --filter "container=${PARENT}" | while read line
 do     
   echo "PARENT DIED $PARENT"
+  echo "Removing the following containers: $CHILD"
   docker rm -f $CHILD || echo "cannot find container $CHILD"
+
+  echo "Removing the following Docker Volumes: $VOLUMES"
+  docker volume rm $VOLUMES
+
   docker rm -f $(hostname)
 done
